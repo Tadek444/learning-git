46be0f7e95a4aa03cc21f91d28beae15efb64f81
Tadeusz Wiazowski <tadeusz.wiazowski@onet.pl>
2020-05-27 18:16:10 +0200

    Add liczby.py

---
+++ liczby.py
@@ -0,0 +1,13 @@
+list_5=[]
+list_q=[]
+for i in range (1,101):    
+    while i%5!=0:
+        i+=1
+        break
+    else:
+        (list_5.append(i))
+        (list_q.append(i**3))
+print('Liczby co 5')
+print (list_5)    
+print ('Liczby sześcienne')
+print (list_q)
